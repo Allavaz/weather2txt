9be0a3e4d791de5cf56f6061038fbf9ca464c59c
David Zavalla <davidzavalla@gmail.com>
2018-08-25 01:42:09 -0300

    Multiple improvements
    - Changed minimum refresh interval to 10 minutes as the openweathermap.org API suggests.
    - Now you need to provide your own API key, can get them by registrating on openweathermap.org for free.
    - Now it remembers the last used City ID, press enter to use it.

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ var url
 var txt
 var rate
 var city
-var d = new Date();
+var d = new Date()
+var apikey
+var json = {}
+var noCity = false
+var cityQuestion
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -20,27 +24,79 @@ console.log()
 main()
 
 function main() {
-    rl.question("Enter City ID (from openweathermap.org): ", (answer) => {
-        url = "http://api.openweathermap.org/data/2.5/weather?id=" + answer + "&units=metric&appid=14eac1a1ca632105d940d23677345cb1"
-        request({ url: url, json: true }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                city = body.name
-                askInterval()
-            } else {
+    fs.readFile("./weather2txt-settings.json", function(error, data) {
+        if (error) {
+            noCity = true
+            console.log("Looks like it's the first time you run this program. In order to get started, register at openweathermap.org and get an API key (it's free!)")
+            console.log()
+            askAPI()
+        } else {
+            json = JSON.parse(data)
+            apikey = json.apikey
+            askCity()
+        }
+    })
+}
+
+function askAPI() {
+    rl.question("Enter your API key: ", (answer) => {
+        request({ url: "http://api.openweathermap.org/data/2.5/weather?id=3435910&appid=" + answer, json: true }, function (error, response, body) {
+            if (response.statusCode == 401) {
+                console.log("Wrong API key. Try again.")
                 console.log()
-                console.log("Wrong City ID, try again.")
-                main()
+                askAPI()
+            } else {
+                apikey = answer
+                console.log("Successfully logged in.")
+                console.log()
+                askCity()
             }
         })
+    })
+}
+
+function askCity() {
+    if (noCity) {
+        cityQuestion = "Enter City ID: "
+    } else {
+        cityQuestion = "Enter City ID (" + json.cityname + "): "
+    }
+    rl.question(cityQuestion, (answer) => {
+        if (answer == "") {
+            url = "http://api.openweathermap.org/data/2.5/weather?id=" + json.cityid + "&units=metric&appid=" + json.apikey
+            city = json.cityname
+            askInterval()
+        } else {
+            url = "http://api.openweathermap.org/data/2.5/weather?id=" + answer + "&units=metric&appid=" + apikey
+            request({ url: url, json: true }, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    city = body.name
+                    json.apikey = apikey
+                    json.cityid = answer
+                    json.cityname = city
+                    noCity = false
+                    json = JSON.stringify(json)
+                    fs.writeFile("./weather2txt-settings.json", json, function (error){
+                        
+                    })
+                    askInterval()
+                }
+                else {
+                    console.log()
+                    console.log("Wrong City ID, try again.")
+                    askCity()
+                }
+            })
+        }
         
     })
 }
 
 function askInterval() {
     rl.question("Enter refresh rate (in minutes): ", (answer) => {
-        if (answer < 1) {
+        if (answer < 10) {
             console.log()
-            console.log("Please enter an interval greater than 1 minute.")
+            console.log("Please enter an interval equal or greater than 10 minutes.")
             askInterval()
         } else {
             rate = answer * 60000
@@ -61,13 +117,8 @@ function requestWeather(url) {
                 if (!error) {
                     console.log()
                     console.log("Successfully written to weather.txt.")
-                    if (rate == 60000) {
-                        console.log()
-                        console.log("Refreshing in 1 minute...")
-                    } else {
-                        console.log()
-                        console.log("Refreshing in " + rate/60000 + " minutes...")
-                    }    
+                    console.log()
+                    console.log("Refreshing in " + rate/60000 + " minutes...")   
                 }
             })
         }
